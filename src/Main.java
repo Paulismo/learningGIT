3df9e334d0d700858315862b911a9efda54d9556
Павел Константинов <078023451p@gmail.com>
2018-10-18 09:52:00 +0300

    branch added

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ interface Int1{
 class Testy implements Int1{
   public void Foo(){
         System.out.println(a*hdfg);
-
+      System.out.println(str.length());
     }
   public void Bar(){
         System.out.println(str);
@@ -22,6 +22,8 @@ public class Main  {
    Testy t1= new Testy();
    t1.Foo();
    t1.Bar();
+   Testy t2= new Testy();
+   t2.Bar();
 
     }
 }
